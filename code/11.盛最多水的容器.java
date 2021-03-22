135089280960ac4477ab33a722a77a7210618399
wangqi <1447071035@qq.com>
2021-03-22 23:47:04 +0800

    11

--- code/11.盛最多水的容器.java
+++ code/11.盛最多水的容器.java
@@ -7,7 +7,27 @@
 // @lc code=start
 class Solution {
     public int maxArea(int[] height) {
-
+        int max=0;
+        int begin=0;
+        int width=0;
+        int high=0;
+        int tmp=0;
+        int end=height.length-1;
+        while(begin<end){
+            width=end-begin;
+            if(height[begin]<height[end]){
+                high=height[begin];
+                begin++;
+            }else{
+                high=height[end];
+                end--;
+            }
+            tmp=high*width;
+            if(max<tmp){
+                max=tmp;
+            }
+        }
+        return max;
     }
 }
 // @lc code=end
