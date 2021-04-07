66703b30daea25652c7fab5cb7ccd3fe3c050cd4
wangqi <1447071035@qq.com>
2021-04-07 23:21:28 +0800

    18

--- code/18.四数之和.java
+++ code/18.四数之和.java
@@ -1,3 +1,7 @@
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 /*
  * @lc app=leetcode.cn id=18 lang=java
  *
@@ -6,9 +10,43 @@
 
 // @lc code=start
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
 
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        if (nums.length < 4) {
+            return Collections.emptyList();
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int left = j + 1;
+                int right = nums.length - 1;
+                while (left < right) {
+                    if (nums[i] + nums[j] + nums[left] + nums[right] == target) {
+                        while (left<nums.length-1 && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (right>1 && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        left++;
+                        right--;
+                    } else if (nums[i] + nums[j] + nums[left] + nums[right] > target) {
+                        right--;
+                    } else {
+                        left++;
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
 // @lc code=end
-
