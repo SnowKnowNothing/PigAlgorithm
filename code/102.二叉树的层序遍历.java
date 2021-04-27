5944ebe66280a09a89f266ed1e536c7d49f09f0d
wangqi <1447071035@qq.com>
2021-04-28 00:06:20 +0800

    104

---
+++ code/102.二叉树的层序遍历.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+/*
+ * @lc app=leetcode.cn id=102 lang=java
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int num = 1;
+        while (queue.size() > 0) {
+            List<Integer> nums = new ArrayList<>();
+            for (int i = 1; i <= num; i++) {
+                if (queue.peek().left != null) {
+                    queue.offer(queue.peek().left);
+                }
+                if (queue.peek().right != null) {
+                    queue.offer(queue.peek().right);
+                }
+                nums.add(queue.poll().val);
+            }
+            num = queue.size();
+            res.add(nums);
+        }
+        return res;
+    }
+}
+// @lc code=end
