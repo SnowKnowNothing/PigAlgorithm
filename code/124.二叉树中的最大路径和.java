b97bcbb2b07100ea03f9f14eb484bd65a59d0c1b
wangqi <1447071035@qq.com>
2021-05-08 00:47:47 +0800

    124

--- code/124.二叉树中的最大路径和.java
+++ code/124.二叉树中的最大路径和.java
@@ -6,24 +6,45 @@
 
 // @lc code=start
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
  */
 class Solution {
-    public int maxPathSum(TreeNode root) {
+    int maxValue = Integer.MIN_VALUE;
 
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        countMaxPathSum(root);
+        return maxValue;
     }
+
+    private int countMaxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = countMaxPathSum(root.left);
+        int right = countMaxPathSum(root.right);
+        int sum;
+        if (left < 0 && right >= 0) {
+            sum = right + root.val;
+        } else if (left >= 0 && right < 0) {
+            sum = left + root.val;
+        } else if (left < 0 && right < 0) {
+            sum = root.val;
+        } else {
+            sum = left + right + root.val;
+        }
+        maxValue = Math.max(maxValue, sum);
+        if (Math.max(left, right) < 0) {
+            return root.val;
+        } else {
+            return Math.max(left, right) + root.val;
+        }
+    }
+
 }
 // @lc code=end
-
